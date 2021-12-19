3c0f36f6e1f659d2c21b22853e7b86990e062df0
theoddorrrrr <aidar140702@mail.ru>
2021-12-20 01:18:48 +0600

    Finished landing

---
+++ lesson-7/gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const rimraf = require('rimraf');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload)
+});
+
+//Html
+gulp.task('html', () => {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+})
+
+//Styles
+gulp.task('sass', () => {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()
+            .on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/css'))
+})
+
+//Images
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*.*')
+        .pipe(gulp.dest('build/img'))
+})
+
+//Clear
+gulp.task('clear', function del(cb) {
+    return rimraf('build', cb);
+});
+
+//Watchers
+gulp.task('watch', function() {
+    gulp.watch('src/index.html', gulp.series('html'));
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('src/img/**/*.*', gulp.series('img'));
+});
+
+gulp.task('default', gulp.series(
+    'clear',
+    gulp.parallel('html', 'sass', 'img'),
+    gulp.parallel('watch', 'server')
+    )
+);
